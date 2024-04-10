5f3e7e17d2779603bb17062a2f66594d6c09e17d
LuisitoCodifica <0gluis.dev@gmail.com>
2024-04-10 18:16:17 +0200

    added Makefile, libft.h and a bunch of functions

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 17:40:08 by lolit-go          #+#    #+#             */
+/*   Updated: 2024/04/10 17:40:10 by lolit-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalpha(int c);
+
+int	ft_isdigit(int c);
+
+int	ft_isalnum(int c);
+
+int	ft_isascii(int c);
+
+int	ft_isprint(int c);
+
+int	ft_tolower(int c);
+
+int	ft_toupper(int c);
