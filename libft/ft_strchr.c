f4bf83a2fe8bbfe812f7155bd3395dba0b10bbea
LuisitoCodifica <0gluis.dev@gmail.com>
2024-05-06 21:53:29 +0200

    remake ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,24 +6,26 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 10:25:08 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/04/11 12:02:00 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/05/06 21:51:29 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
 	char	*s1;
+	int		i;
 
 	s1 = (char *) s;
+	i = 0;
 	if (c > 127)
 		c %= 256;
-	while (*s1)
+	while (i <= ft_strlen(s1))
 	{
-		if (c == *s1)
-			return (s1);
-		s1++;
+		if (c == s1[i])
+			return (s1 + i);
+		i++;
 	}
-	if (c == *s1)
-		return (s1);
 	return (0);
 }
