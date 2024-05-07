9fdbeb60f3be426f1342a18c536bd5a4bea9eb62
LuisitoCodifica <0gluis.dev@gmail.com>
2024-05-07 03:31:18 +0200

    fixed ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,19 +6,18 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 13:40:01 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/04/11 14:23:04 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/05/07 03:30:49 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 int	ft_strlcpy(char *dst, const char *src, int size)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] && i < size - 1)
+	while (src[i] && (i < size - 1))
 	{
 		if (size == 0)
 			break ;
@@ -27,8 +26,5 @@ int	ft_strlcpy(char *dst, const char *src, int size)
 	}
 	if (i < size)
 		dst[i] = '\0';
-	i = 0;
-	while (src[i])
-		i++;
-	return (i);
+	return (ft_strlen(src));
 }
