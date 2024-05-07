a8a90dbf2bd939959a13d6cfdbd4061461860b41
LuisitoCodifica <0gluis.dev@gmail.com>
2024-05-07 02:00:02 +0200

    fixed ft_calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:54:13 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/05/06 17:59:49 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/05/07 01:56:32 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(int nmemb, int size)
 	void	*ptr;
 
 	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (0);
 	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
