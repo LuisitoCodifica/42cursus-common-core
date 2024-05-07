19554e7c3c87062e02f4148e623f6dc287a9d2b0
LuisitoCodifica <0gluis.dev@gmail.com>
2024-05-07 02:40:58 +0200

    fixed ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,18 +6,25 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:45:32 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/04/12 15:54:01 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/05/07 02:39:18 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memcpy(void *dst, const void *src, int n)
 {
+	unsigned char	*dst_ptr;
+	unsigned char	*src_ptr;
 	int				i;
-	unsigned char	*ptr;
 
-	ptr = (unsigned char *) dst;
+	dst_ptr = (unsigned char *) dst;
+	src_ptr = (unsigned char *) src;
+	if (!dst_ptr && !src_ptr)
+		return (0);
 	i = 0;
-	while ((src + i) && i < n)
-		ptr[i++] = *(unsigned char *) src++;
-	return (dst);
+	while (i < n)
+	{
+		dst_ptr[i] = src_ptr[i];
+		i++;
+	}
+	return (dst_ptr);
 }
