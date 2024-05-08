064c2e44c8a7f0810c920d3954e9dfdc223ee89d
LuisitoCodifica <0gluis.dev@gmail.com>
2024-05-08 03:16:46 +0200

    remade ft_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,63 +6,44 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 13:17:27 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/04/15 14:35:37 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/05/08 03:10:51 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_check_is_valid(int c, int next)
+static int	ft_isspace(int c)
 {
-	if (c == ' ')
-		return (203);
-	if ((c >= '\011' && c <= '\015')
-		|| c == '+')
-		return (202);
-	if (!ft_isdigit(c))
-		return (201);
-	if (ft_isdigit(c) && !ft_isdigit(next))
-		return (200);
+	if (c == '\t'
+		|| c == '\n'
+		|| c == '\v'
+		|| c == '\f'
+		|| c == '\r'
+		|| c == ' ')
+		return (1);
 	return (0);
-}
-
-static int	ft_convert_to_num(const char *nptr)
-{
-	int	n;
-	int	check;
-
-	n = 0;
-	while (*nptr)
-	{
-		check = ft_check_is_valid(*nptr, *(nptr + 1));
-		if (check == 203 || check == 202)
-		{
-			nptr++;
-			continue ;
-		}
-		if (check == 201)
-			return (n);
-		n = n * 10 + *nptr - '0';
-		if (check == 200)
-			return (n);
-		nptr++;
-	}
-	return (n);
 }
 
 int	ft_atoi(const char *nptr)
 {
-	int	neg;
+	int	n;
+	int	sign;
 
-	neg = 0;
-	if ((*nptr == '-' || *nptr == '+') && !ft_isdigit(*(nptr + 1)))
-		return (0);
-	if (*nptr == '-')
+	n = 0;
+	sign = 1;
+	while (*nptr)
 	{
-		neg = 1;
+		if (!ft_isspace(*nptr))
+			break ;
 		nptr++;
 	}
-	if (neg)
-		return (ft_convert_to_num(nptr) * -1);
-	return (ft_convert_to_num(nptr));
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr && ft_isdigit(*nptr))
+		n = n * 10 + (*(nptr++) - '0');
+	return (sign * n);
 }
