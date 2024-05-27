8eeb351e33ae5bc64f976fb31f847b71dac33eb6
LuisitoCodifica <0gluis.dev@gmail.com>
2024-05-27 19:46:46 +0200

    added ft_split (finally)

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,83 +6,96 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 02:47:37 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/05/21 01:07:15 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/05/27 19:45:07 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "libft.h"
 
-static char	*ft_parse_array(int *size, const char *s, char c)
+static int	ft_count_words(const char *s, char c)
 {
-	char	*str;
-	char	c_string[2];
+	int		words;
 	int		i;
 
-	str = ft_strdup("");
-	c_string[0] = c;
-	c_string[1] = '\0';
-	*size = 0;
+	words = 0;
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] != c)
 		{
 			while (s[i] && s[i] != c)
-				str = ft_strjoin(str, ft_substr(s, i++, 1));
-			str = ft_strjoin(str, c_string);
-			*size += 1;
+				i++;
+			words += 1;
 		}
 		else
 			i++;
 	}
-	return (str);
+	return (words);
+}
+
+static void	ft_parse_array(const char *s, char c, int **strs_pos, int size)
+{
+	int		i;
+	int		j;
+
+	*strs_pos = (int *) malloc((size * 2) * sizeof(int));
+	if (*strs_pos)
+	{
+		j = 0;
+		i = 0;
+		while (s[i])
+		{
+			if (s[i] != c)
+			{
+				(*strs_pos)[j] = i;
+				while (s[i] && s[i] != c)
+					i++;
+				(*strs_pos)[j + size] = (i - (*strs_pos)[j]);
+				j++;
+			}
+			else
+				i++;
+		}
+	}
+}
+
+static char	**ft_empty_array(void)
+{
+	char	**array;
+
+	array = (char **) malloc(sizeof(char *));
+	if (!array)
+		return (NULL);
+	array[0] = NULL;
+	return (array);
 }
 
 char	**ft_split(const char *s, char c)
 {
 	char	**array;
-	char	*str;
+	int		*strs_pos;
 	int		size;
-	int		len;
 	int		i;
 
-	str = ft_parse_array(&size, s, c);
+	size = ft_count_words(s, c);
+	if (size == 0)
+		return (ft_empty_array());
+	ft_parse_array(s, c, &strs_pos, size);
+	if (!strs_pos)
+		return (NULL);
 	array = (char **) malloc((size + 1) * sizeof(char *));
 	if (!array)
-		return (0);
+		return (NULL);
 	i = 0;
 	while (i < size)
 	{
-		len = (ft_strchr(str, c) - str);
-		array[i] = (char *) malloc((len + 1) * sizeof(char));
-		if (!array[i])
-			return (0);
-		ft_strlcpy(array[i], str, (len + 1));
-		str = (str + len + 1);
+		array[i] = (char *) malloc((strs_pos[i + size] + 1) * sizeof(char));
+		ft_strlcpy(array[i], (s + strs_pos[i]), (strs_pos[i + size] + 1));
 		i++;
 	}
 	array[i] = 0;
+	free(strs_pos);
 	return (array);
-}
-
-int main() {
-	// char *s = "      split       this for   me  !       ";
-	char *s = "xxxxxxxxhello!";
-
-	char **result = ft_split(s, 'x');
-	if (!result)
-		printf("TEST_FAILED\n");
-	else
-		printf("TEST_SUCCESS\n");
-	
-	int i = 0;
-	while (result[i])
-	{
-		printf("%2d: (%s)\n", i + 1, result[i]);
-		i++;
-	}
-
-	return (0);
 }
