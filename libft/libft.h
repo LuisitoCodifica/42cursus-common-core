a973294c690f32026c31b9cfb3298d2f88e8b801
LuisitoCodifica <0gluis.dev@gmail.com>
2024-08-22 15:09:29 +0200

    added ft_lstadd_front

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 17:40:08 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/08/22 14:51:53 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/08/22 15:00:09 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,5 +91,7 @@ typedef struct s_list
 }					t_list;
 
 t_list	*ft_lstnew(void *content);
+
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
