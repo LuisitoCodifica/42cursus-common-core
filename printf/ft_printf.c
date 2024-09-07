d145d84aa4caff3d13f3db48d489cc04e70fba58
Luis Olit Gomez <lolit-go@c3r7s4.42malaga.com>
2024-09-07 18:07:18 +0200

    basic structure

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,12 +6,38 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 15:53:22 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/09/07 16:17:09 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/09/07 18:05:46 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(int c)
+{
+	write(0, &c, 1);
+}
+
 int	ft_printf(const char *s, ...)
 {
-	s = s + 1;
-	return (0);
+	int	count;
+
+	write(0, "\"", 1);
+	count = 0;
+	while (*s)
+	{
+		if (*s == '%')
+		{
+			s++;
+			if (*s == '%')
+				ft_putchar('%');
+			else
+				s--;
+		}
+		else
+			ft_putchar(*s);
+		count++;
+		s++;
+	}
+	write(0, "\"", 1);
+	return (count);
 }
---
+++ printf/main.c
@@ -0,0 +1,7 @@
+#include "ft_printf.h"
+#include "stdio.h"
+
+int main() {
+	printf("\nchars: %d\n", ft_printf(" %% "));
+	return 0;
+}
