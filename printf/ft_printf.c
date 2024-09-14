d51808ca6430c6ff79085af95280560b1829e919
LuisitoCodifica <0gluis.dev@gmail.com>
2024-09-14 23:38:56 +0200

    implemented error handling

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,42 +6,86 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 15:53:22 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/09/12 00:47:30 by lolit-go         ###   ########.fr       */
+/*   Updated: 2024/09/14 23:35:47 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *s, ...)
+static int	ft_validate(const char *s, const char *specifiers)
 {
-	int		num_chars;
-	va_list	params;
+	int	i;
 
-	num_chars = 0;
-	va_start(params, s);
+	i = 0;
 	while (*s)
 	{
 		if (*s == '%')
 		{
 			s++;
+			while (specifiers[i])
+			{
+				if (*s == specifiers[i])
+					return (0);
+				i++;
+			}
+			i = 0;
+		}
+		s++;
+	}
+	return (1);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	char	*str;
+	int		num_chars;
+	int		clear;
+	va_list	params;
+
+	if (ft_validate(s, "cspdixX%"))
+		return (-1);
+	num_chars = 0;
+	va_start(params, s);
+	while (*s)
+	{
+		str = NULL;
+		clear = 1;
+		if (*s == '%')
+		{
+			s++;
 			if (*s == 'c')
-				num_chars += ft_putchar(va_arg(params, int));
+			{
+				ft_putchar(va_arg(params, int));
+				clear = 0;
+			}
 			else if (*s == 's')
-				num_chars += ft_putstr(va_arg(params, char *));
+			{
+				str = va_arg(params, char *);
+				clear = 0;
+			}
 			else if (*s == 'p')
-				num_chars += ft_putstr(ft_mem_addr(va_arg(params, void *)));
+				str = ft_mem_addr(va_arg(params, void *));
 			else if (*s == 'd')
-				num_chars += ft_putstr(ft_itoa_base(va_arg(params, int), 10, 0));
+				str = ft_itoa_base(va_arg(params, int), 10, 0);
 			else if (*s == 'i')
-				num_chars += ft_putstr(ft_itoa_base(va_arg(params, int), 10, 0));
+				str = ft_itoa_base(va_arg(params, int), 10, 0);
 			else if (*s == 'u')
-				num_chars += ft_putstr(ft_uitoa(va_arg(params, unsigned int)));
+				str = ft_uitoa(va_arg(params, unsigned int));
 			else if (*s == 'x')
-				num_chars += ft_putstr(ft_itoa_base(va_arg(params, int), 16, 0));
+				str = ft_itoa_base(va_arg(params, int), 16, 0);
 			else if (*s == 'X')
-				num_chars += ft_putstr(ft_itoa_base(va_arg(params, int), 16, 1));
+				str = ft_itoa_base(va_arg(params, int), 16, 1);
 			else if (*s == '%')
-				num_chars += ft_putchar('%');
+			{
+				ft_putchar('%');
+				clear = 0;
+			}
+			if (!str)
+				num_chars += 1;
+			else
+				num_chars += ft_putstr(str);
+			if (clear)
+				free(str);
 		}
 		else
 			num_chars += ft_putchar(*s);
--- printf/main.c
+++ printf/main.c
@@ -4,18 +4,25 @@
 int main() {
 	int x = 42;
 	int *px = &x;
-	// ft_printf(" %c ", 'e');
-	// printf("\nchars: %d\n", ft_printf(" %c ", 'e'));
-	// printf("\nchars: %d\n", ft_printf(" %s !!! ", "hola"));
-	// printf("\nchars: %d\n\n", ft_printf(" %% %c%c %s !!! ", 'e', 'y', "hola"));
-	// printf("\nchars: %d\n\n", ft_printf(" %x %X ", 1100, 1112));
-	// printf(" %x \n\n", 1100);
+	ft_printf("\" %c \"\n", 'e');
+	ft_printf("\" %s !!! \"\n", "hola");
+	ft_printf("\" %% %c%c %s !!! \"\n", 'e', 'y', "hola");
+	ft_printf("\" %x %X \"\n", 1100, 1100);
+	ft_printf("\" %d -- (%d)  -- %d \"\n", 286354, -1876763, 0);
+	ft_printf("\" %i -- (%i)  -- %i \"\n", 286354, -1876763, 0);
+	ft_printf("\"\t%s\"\n\n", "holi");
 
-	// printf("va: %p\n", px);
-	// ft_printf("ft: %p\n", px);
+	printf("va: %p\n", px);
+	ft_printf("ft: %p\n\n", px);
 
-	printf("va: %u\n", 623548126);
-	ft_printf("ft: %u\n", 623548126);
+	printf("va: %u\n", 4294967293);
+	ft_printf("ft: %u\n", 4294967293);
+
+	printf("\n\"%-4d\"\n\n", 3030);
+
+	// printf("chars: %d\n\n", printf("ey %wd holaaa", 33));
+
+	printf("\nchars: %d\n\n", ft_printf("ey %wc holaaa", 33));
 
 	return 0;
 }
