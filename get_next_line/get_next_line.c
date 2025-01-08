3b9fec1250683f4ceb7e1466fd4fd0893f93e1be
Luis Olit Gomez <lolit-go@c2r8s1.42malaga.com>
2025-01-08 16:05:19 +0100

    gnl remake

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,45 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/22 15:04:05 by lolit-go          #+#    #+#             */
-/*   Updated: 2024/10/24 18:08:26 by lolit-go         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	static char		buffer[BUFFER_SIZE + 1];
-	static char		*line;
-	static size_t	line_len;
-	ssize_t			bytes_read;
+	static char	buffer[BUFFER_SIZE + 1];
+	ssize_t		read_bytes;
 
-	// Detectar si hay sobrante y pasarlo a line
-
-	// Leer fd y pasarlo al buffer
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read == -1)
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	if (read_bytes == -1)
 		return (NULL);
-	buffer[bytes_read] = '\0';
+	buffer[read_bytes] = 0;
 
-	// Calcular line_len
-	line_len = ft_get_len(line);
-
-	// Realloc con tamaño de buffer a line
-	line = (char *) ft_realloc(line, (line_len * sizeof(char)), ((line_len + bytes_read) * sizeof(char)));
-	if (!line)
-		return (NULL);
-
-	// Copiar buffer a line
-		// Si uno de los carácteres es '\n' retornar line
-		// Si se llega al límite del buffer iterar función
-
-	// Si hay sobrante en el buffer ponerlo al principio e iterar función
-
-	return (NULL);
+	return (buffer);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,32 +1,14 @@
 #include "get_next_line.h"
 
 int main(int argc, char **argv) {
-	if (argc < 3)
-		return 1;
-
+	if (argc != 2)
+		exit(1);
 	int fd = open(argv[1], O_RDONLY);
 	char *str;
-	// int i = argv[2][0] - '0';
+	
+	str = get_next_line(fd);
 
-	// while (i) {
-	// 	get_next_line(fd);
-	// 	sleep(1);
-	// 	i--;
-	// }
+	printf("%s\n", str);
 
-	// printf("\n---\n\n");
-
-	int i = 0;
-	int n = argv[2][0] - '0';
-	while (i < n)
-	{
-		str = get_next_line(fd);
-		printf("%s\n", str);
-		i++;
-	}
-
-	free(str);
-	close(fd);
-
-	return 0;
+	return (0);
 }
