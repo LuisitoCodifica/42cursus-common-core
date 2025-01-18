f853c5cc1454b5935564b3f18cc4a21c3ff57d28
Luis Olit Gomez <lolit-go@c1r14s1.42malaga.com>
2025-01-18 21:14:07 +0100

    gnl prog

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,36 +6,43 @@
 /*   By: lolit-go <lolit-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 20:22:47 by lolit-go          #+#    #+#             */
-/*   Updated: 2025/01/18 20:54:11 by lolit-go         ###   ########.fr       */
+/*   Updated: 2025/01/18 21:12:54 by lolit-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*_buf_parse(char *buffer, ssize_t read_bytes)
+static char	*_buf_parse(char *buffer)
 {
-	char	*aux;
+	char	*str;
+	ssize_t	len;
 	ssize_t	i;
 
 	// printf("buffer:\n%s\n", buffer);
 	for (int i = 0; buffer[i]; i++)
 		printf("%c", buffer[i]);
 
-	aux = malloc((read_bytes + 1) * sizeof(char));
-	if (!aux)
+	len = 0;
+	while (buffer[len])
+	{
+		len++;
+		if (buffer[len] == '\n')
+			break ;
+	}
+
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
 
 	i = 0;
-	while (i < read_bytes)
+	while (i <= len)
 	{
-		aux[i] = buffer[i];
-		if (buffer[i] == '\n')
-			break ;
+		str[i] = buffer[i];
 		i++;
 	}
-	aux[i] = '\0';
+	str[i] = '\0';
 
-	return (aux);
+	return (str);
 }
 
 static int	_buf_read(t_list **lst, int fd)
@@ -49,12 +56,25 @@ static int	_buf_read(t_list **lst, int fd)
 		return (R_FAILURE);
 	buffer[read_bytes] = 0;
 
-	content = _buf_parse(buffer, read_bytes);
+	content = _buf_parse(buffer);
 	if (!content)
 		return (R_FAILURE);
 	ft_lstadd_back(&(*lst), ft_lstnew(content));
 
 	return (R_SUCCESS);
+}
+
+static void __print_lst(t_list *lst, int fd)
+{
+	_buf_read(&lst, fd);
+		printf(RED "\n\nres: " RESET);
+
+	if (lst && lst->content) {
+		for (int i = 0; ((char *) lst->content)[i] != '\0'; i++) {
+			printf("%c", ((char *) lst->content)[i]);
+		}
+		printf("\n\n");
+	}
 }
 
 char	*get_next_line(int fd)
@@ -65,38 +85,10 @@ char	*get_next_line(int fd)
 	// if (_buf_read(&lst, fd))
 	// 	return (NULL);
 
-	_buf_read(&lst, fd);
-		printf("\n");
-
-	if (lst && lst->content) {
-		for (int i = 0; ((char *) lst->content)[i] != '\0'; i++) {
-			printf("%c", ((char *) lst->content)[i]);
-		}
-		printf("\n\n");
-	}
-
-	_buf_read(&lst, fd);
-		printf("\n");
-
-	if (lst && lst->content) {
-		lst = lst->next;
-		for (int i = 0; ((char *) lst->content)[i] != '\0'; i++) {
-			printf("%c", ((char *) lst->content)[i]);
-		}
-		printf("\n\n");
-	}
-
-	_buf_read(&lst, fd);
-		printf("\n");
-
-	if (lst && lst->content) {
-		lst = lst->next;
-		for (int i = 0; ((char *) lst->content)[i] != '\0'; i++) {
-			printf("%c", ((char *) lst->content)[i]);
-		}
-		printf("\n\n");
-	}
-
+	__print_lst(lst, fd);
+	__print_lst(lst, fd);
+	__print_lst(lst, fd);
+	
 
 	return (line);
 }
